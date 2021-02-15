03d21a0efad65d9e68ee2210b6247f472a67529d
SideDot <78238680+SideDot@users.noreply.github.com>
2021-02-15 18:11:02 +0100

    Add files via upload

---
+++ PyCalc.py
@@ -0,0 +1,27 @@
+#Scaramessage.bot-A Scarabots inc. property, all rights reserved(2020-2025)!PRIVATE USE ONLY!
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#Bozze progetti, quando hai finito, elimina la bozza fino a qui dopo averla esportata sul documento
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+
+
+            
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
